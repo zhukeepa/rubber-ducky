1fafff2d41d99b726fb64d144ab4f55fbcd66437
Alex <zhukeepa@gmail.com>
2015-07-06 22:20:21 -0700

    fix gmail oauth scope

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, ENV['CLIENT_ID'], ENV['CLIENT_SECRET'], {
-  scope: ['email',
-    'https://www.googleapis.com/auth/gmail.modify'],
-    access_type: 'offline'}
+  provider :google_oauth2, ENV['CLIENT_ID'], ENV['CLIENT_SECRET'], 
+  { scope: ['email', 'profile', 'gmail.modify'], access_type: 'offline'}
 end
